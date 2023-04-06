4d0ae58738bd7962d404fbe71ce331d133d5e55c
phongphong.28 <119289183+P-ro-VL@users.noreply.github.com>
2023-04-06 20:08:10 +0700

    commit

--- public/index.js
+++ public/index.js
@@ -55,83 +55,31 @@ function register(email, password) {
       // ..
     });
 }
+import { getFirestore, collection, addDoc, getDocs } from "firebase/firestore";
 
-const admin = require("firebase-admin");
+// Get Firestore instance
+const db = getFirestore(app);
 
-// Initialize Firebase Admin SDK
-const serviceAccount = require("/path/to/serviceAccountKey.json");
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-// Get a Firestore instance
-const db = admin.firestore();
-
-// Function to add data to Firestore
-function addDataToFirestore(collectionName, documentName, data) {
-  const docRef = db.collection(collectionName).doc(documentName);
-  return docRef
-    .set(data)
-    .then(() => {
-      console.log("Data added to Firestore");
-    })
-    .catch((error) => {
-      console.error("Error adding data to Firestore: ", error);
-    });
+// Write data to Firestore
+async function writeData(collectionName, data) {
+  try {
+    const docRef = await addDoc(collection(db, collectionName), data);
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
 }
 
-// Function to read data from Firestore
-function readDataFromFirestore(collectionName) {
-  return db
-    .collection(collectionName)
-    .get()
-    .then((querySnapshot) => {
-      const data = [];
-      querySnapshot.forEach((doc) => {
-        data.push({ id: doc.id, ...doc.data() });
-      });
-      return data;
-    })
-    .catch((error) => {
-      console.error("Error reading data from Firestore: ", error);
-    });
+async function readDataById(collectionName, docId) {
+  try {
+    const docRef = doc(db, collectionName, docId);
+    const docSnapshot = await getDoc(docRef);
+    if (docSnapshot.exists()) {
+      console.log(`${docSnapshot.id} => ${JSON.stringify(docSnapshot.data())}`);
+    } else {
+      console.log("No such document!");
+    }
+  } catch (e) {
+    console.error("Error getting document: ", e);
+  }
 }
-
-// Function to get document data of a person with a given name
-function getPersonDataByName(collectionName, name) {
-  return db
-    .collection(collectionName)
-    .where("name", "==", name)
-    .get()
-    .then((querySnapshot) => {
-      const data = [];
-      querySnapshot.forEach((doc) => {
-        data.push({ id: doc.id, ...doc.data() });
-      });
-      return data;
-    })
-    .catch((error) => {
-      console.error("Error getting person data from Firestore: ", error);
-    });
-}
-
-// Example usage
-const data = {
-  name: "John Doe",
-  email: "johndoe@example.com",
-  age: 30,
-};
-addDataToFirestore("users", "user1", data)
-  .then(() => {
-    return readDataFromFirestore("users");
-  })
-  .then((data) => {
-    console.log("Data read from Firestore: ", data);
-    return getPersonDataByName("users", "John Doe");
-  })
-  .then((data) => {
-    console.log("Person data read from Firestore: ", data);
-  })
-  .catch((error) => {
-    console.error("Error: ", error);
-  });
